4916d240548aebe75d265067c34dd03bbb07106d
Scott <swanninger@gmail.com>
2018-08-22 12:59:51 -0500

    init

---
+++ src/main/java/com/fresh/alohainjector/AlohaInjectorApplication.java
@@ -0,0 +1,12 @@
+package com.fresh.alohainjector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlohaInjectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlohaInjectorApplication.class, args);
+    }
+}
